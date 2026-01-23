ed5fd84d521d280114324c7f2ff1f267515ea149
RollieGarcia0031 <garciarollie827@gmail.com>
2026-01-23 10:21:34 +0800

    fixed output for ai/meal-recipe

--- app/api/ai/meal-recipe/route.ts
+++ app/api/ai/meal-recipe/route.ts
@@ -1,7 +1,44 @@
 import { NextResponse } from "next/server";
+import { createSupabaseServerClient as supabase } from "@/lib/supabase/server";
+import { getFullRecipeFlow } from "@/lib/ai-flow/getFullRecipe";
+/**
+ * 
+ * Request: {
+ *  recipe_name: "egg pie",
+ *  ingredients: ["egg", "sugar", "flour"]
+ * }
+ */
+export async function POST(req: Request){
+  try {
+    const bearer = req.headers.get('Authorization');
 
-export function POST(req: Request){
-  return NextResponse.json({
+    if (!bearer) return new NextResponse(null,{status: 401});
 
-  });
+    const token = bearer.replace("Bearer ", '');
+    const {data, error} = await (await supabase()).auth.getUser(token);
+
+    if (error || !data.user.id) return new NextResponse(null, {status: 401});
+
+    const { ingredients, recipe_name } = await req.json();    
+    
+    if (!recipe_name || !Array.isArray(ingredients)) {
+      return NextResponse.json(
+        { error: "Invalid request body" },
+        { status: 400 }
+      );
+    }
+
+    const result = await getFullRecipeFlow({ingredients, recipe_name});
+
+    return new NextResponse(JSON.stringify(result), {
+      status: 200
+    })
+    
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Error occured during instruction generation", {
+      status: 500
+    })
+  }
+
 }
