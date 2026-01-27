60fe164a0b99baf3dd95bff37778e4e17c9b4678
RollieGarcia0031 <garciarollie827@gmail.com>
2026-01-27 20:53:13 +0800

    add ui for adding ingredient

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,55 @@
-import Image from "next/image";
+"use client";
+
+import { supabase } from "@/lib/supabase/client";
+import { create } from "domain";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+
+  useEffect(()=>{
+    async function fetchIngredients(){
+      const data = await getAllIngredients();
+      setIngredients(data);
+    }
+
+    fetchIngredients();
+  },[]);
+
   return (
     <div className="card-screen">
-      <main className="card">
-        <h1>hello</h1>
-      </main>
+      <div>
+        
+        <div className="w-full">
+          <select
+            className="w-full bg-bg-light border-border border border-solid py-2 px-4 rounded-sm">
+            {ingredients.map((ingredient) => (
+              <option key={ingredient.id} value={ingredient.id}>
+                {ingredient.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button className="mt-4 bg-bg-light border-border border border-solid w-[20rem]
+          py-2 px-4 rounded-sm hover:bg-highlight duration-150">
+          Add
+        </button>
+      </div>
     </div>
   );
+}
+
+interface Ingredient {
+  category: string;
+  created_at: string;
+  id: string;
+  name: string;
+  normalized_name: string;
+}
+async function getAllIngredients(): Promise<Ingredient[]> {
+  const { data, error } = await supabase.from("ingredients").select("*");
+  if (error) throw new Error(error.message);
+  return data;
 }
