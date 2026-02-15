cf5087a308466c550dbe816eb41ec78cd49ca63c
RollieGarcia0031 <garciarollie827@gmail.com>
2026-02-15 16:45:04 +0800

    add logout route

---
+++ app/auth/logout/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase/client"
+import { useRouter } from "next/navigation";
+import { Puff } from "react-loader-spinner";
+
+export default function Logout(){
+
+  const router = useRouter();
+
+  useEffect(()=>{
+    const logout = async ()=>{
+      await supabase.auth.signOut();
+      router.push("/auth/login");
+    }
+
+    logout();
+
+    return ()=>{
+      logout();
+    }
+  },[]);
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center gap-4">
+      <Puff
+        visible={true}
+        height="80"
+        width="80"
+        color="#f57e42"
+        ariaLabel="puff-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+      <p className="text-text-muted">
+        Logging out ...
+      </p>
+    </div>
+  )
+}
